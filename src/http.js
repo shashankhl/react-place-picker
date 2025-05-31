13f917594049c9b2319a466b0aa09cbfa76174f8
shashankhl <shashankhl280@gmail.com>
2025-05-31 17:25:32 +0530

    deploy commit

--- src/http.js
+++ src/http.js
@@ -1,38 +1,45 @@
 export async function fetchAvailablePlaces() {
-  const response = await fetch('http://localhost:3000/places');
+  const response = await fetch(
+    "https://react-place-picker-backend.onrender.com/places"
+  );
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error('Failed to fetch places');
+    throw new Error("Failed to fetch places");
   }
 
   return resData.places;
 }
 
 export async function fetchUserPlaces() {
-  const response = await fetch('http://localhost:3000/user-places');
+  const response = await fetch(
+    "https://react-place-picker-backend.onrender.com/user-places"
+  );
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error('Failed to fetch user places');
+    throw new Error("Failed to fetch user places");
   }
 
   return resData.places;
 }
 
 export async function updateUserPlaces(places) {
-  const response = await fetch('http://localhost:3000/user-places', {
-    method: 'PUT',
-    body: JSON.stringify({ places }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const response = await fetch(
+    "https://react-place-picker-backend.onrender.com/user-places",
+    {
+      method: "PUT",
+      body: JSON.stringify({ places }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error('Failed to update user data.');
+    throw new Error("Failed to update user data.");
   }
 
   return resData.message;
